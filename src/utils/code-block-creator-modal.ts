db30c6dd1705ea285e85125f02b19bc1d938d99d
altarok <thom.hammerklinge@gmail.com>
2026-06-23 13:12:16 +0200

    change some strings seen by user

--- src/utils/code-block-creator-modal.ts
+++ src/utils/code-block-creator-modal.ts
@@ -5,7 +5,7 @@ import {ATOM_VISUALIZATION, MOLECULE_THEMES, SHOW_CARBONS} from '../definitions/
 import {GenericModal, MandatoryInput, OptionalInput, OutputData} from '@Altarok/obsidian-dev-utils/src'
 import SmilesDrawer from 'smiles-drawer'
 
-// npm update @Altarok/obsidian-dev-utils
+// npm link/update @Altarok/obsidian-dev-utils
 export class CodeBlockCreatorModal extends Modal {
   constructor(public readonly app: App, public readonly plugin: SmilesDrawerToObsidianPlugin) {
     super(app)
@@ -106,7 +106,6 @@ function createMandatoryInput(): Readonly<MandatoryInput>[] {
     key: 'smiles',
     ignoreKeyInCodeBlock: true,
     current: '',
-    tooltip: 'This will apply a theme to your code block.'
   }]
 }
 
@@ -116,19 +115,19 @@ function createOptionalInput(globalSettings: Readonly<PluginSettings>): Readonly
       type: 'expandable', prompt: 'Colors and Themes',
       nestedInput: [
         {
-          type: 'dropdown', prompt: 'Select theme.', key: 'theme',
-          tooltip: 'This will apply a theme to your molecule.',
+          type: 'dropdown', prompt: 'Theme', key: 'theme',
+          tooltip: 'Apply theme to your code block.',
           current: globalSettings.theme,
           dropdownOptions: MOLECULE_THEMES as readonly string[]
         },
         {
-          type: 'color', prompt: 'Change background color.', key: 'backgroundColor',
+          type: 'color', prompt: 'Background color', key: 'backgroundColor',
           current: globalSettings.backgroundColor,
         },
       ]
     },
     {
-      type: 'expandable', prompt: 'Show atom options',
+      type: 'expandable', prompt: 'Visuals',
       nestedInput: [
         {
           type: 'dropdown', prompt: 'How to visualize atoms', key: 'atomVisualization',
@@ -165,42 +164,50 @@ function createOptionalInput(globalSettings: Readonly<PluginSettings>): Readonly
       type: 'expandable', prompt: 'Advanced',
       nestedInput: [
         {
-          type: 'slider', prompt: 'Change padding.', key: 'padding',
+          type: 'slider', prompt: 'Padding', key: 'padding',
           from: 0, to: 50, step: 5, current: globalSettings.padding,
         },
         {
-          type: 'slider', prompt: 'Change atom font size.', key: 'fontSizeLarge',
+          type: 'slider', prompt: 'Atom font size', key: 'fontSizeLarge',
           from: 5, to: 20, step: 1, current: globalSettings.fontSizeLarge,
         },
         {
-          type: 'slider', prompt: 'Change counter font size.', key: 'fontSizeSmall',
+          type: 'slider', prompt: 'Counter font size', key: 'fontSizeSmall',
           from: 2, to: 10, step: 1, current: globalSettings.fontSizeSmall,
         },
         {
-          type: 'slider', prompt: 'Change bond thickness.', key: 'bondThickness',
+          type: 'slider', prompt: 'Bond thickness', key: 'bondThickness',
           from: 0.5, to: 3, step: 0.1, current: globalSettings.bondThickness,
         },
         {
-          type: 'slider', prompt: 'Change bond length.', key: 'bondLength',
+          type: 'slider', prompt: 'Bond length', key: 'bondLength',
           tooltip: 'Distance between atoms.',
           from: 5, to: 100, step: 5, current: globalSettings.bondLength,
         },
         {
-          type: 'slider', prompt: 'Change short bond length.', key: 'shortBondLength',
-          tooltip: 'This is the length of relative to the normal bond length.',
+          type: 'slider', prompt: 'Relative length of short bond', key: 'shortBondLength',
+          tooltip: 'Length of short bond relative to normal bond.',
           from: 0.5, to: 1, step: 0.1, current: globalSettings.shortBondLength,
         },
         {
-          type: 'slider', prompt: 'Change bond spacing.', key: 'bondSpacing',
+          type: 'slider', prompt: 'Bond spacing', key: 'bondSpacing',
           from: 1, to: 10, step: 0.1, current: globalSettings.bondSpacing,
         },
+        // {
+        //   type: 'slider', prompt: 'Width. Will only apply in note.', key: 'width',
+        //   from: 50, to: 1000, step: 10, current: globalSettings.width,
+        // },
         {
-          type: 'slider', prompt: 'Change width.', key: 'width',
-          from: 50, to: 1000, step: 10, current: globalSettings.width,
+          type: 'slider', prompt: 'Height. Will only apply in note.', key: 'height',
+          from: 50, to: 1000, step: 10, current: globalSettings.height,
         },
         {
-          type: 'slider', prompt: 'Change height.', key: 'height',
-          from: 50, to: 1000, step: 10, current: globalSettings.height,
+          type: 'slider', prompt: 'Overlap sensitivity', key: 'overlapSensitivity',
+          from: 0.1, to: 1, step: 0.01, current: globalSettings.overlapSensitivity,
+        },
+        {
+          type: 'slider', prompt: 'Overlap resolution iterations', key: 'overlapResolutionIterations',
+          from: 1, to: 10, step: 1, current: globalSettings.overlapResolutionIterations,
         },
       ]
     },
