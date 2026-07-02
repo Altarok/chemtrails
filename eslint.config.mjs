d02d8a7c53c190b60b057ac724c7e594b0d71d1a
altarok <thom.hammerklinge@gmail.com>
2026-07-02 11:27:12 +0200

    chore: replace tabs with spaces and equalize eslint configs over all projects

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -63,18 +63,6 @@ export default [
         }
       ]
 
-      // "@typescript-eslint/no-redundant-type-constituents": "error", // catch redundant types like 'Error | unknown'
-      // "@typescript-eslint/no-explicit-any": "warn",
-      // "no-undef": "error",
-      // "@typescript-eslint/no-misused-promises": ["error", {"checksVoidReturn": true}],
-      // "no-unused-vars": "off",
-      // "@typescript-eslint/no-unused-vars": ["error", {"argsIgnorePattern": "^_", "varsIgnorePattern": "^_"}], // Turned off to let the TypeScript-specific rule handle it
-      // "no-shadow": "off",
-      // "@typescript-eslint/no-shadow": "error", // Triggers an error if you reuse a variable name from an outer scope
-      // "@typescript-eslint/prefer-optional-chain": "error", // Enforces safe optional chaining or nullish coalescing instead of unsafe casting
-      // "@typescript-eslint/prefer-nullish-coalescing": "error",
-      // "prefer-template": "warn", // Flags old-school string concatenation in favor of template strings
-
     }
   }
 ];
