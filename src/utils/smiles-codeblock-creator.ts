3facf613587ccda3e5d7ab55f9f907df41705b9a
Altarok <thom.hammerklinge@gmail.com>
2026-07-21 20:38:39 +0200

    update ref to own util lib

--- src/utils/smiles-codeblock-creator.ts
+++ src/utils/smiles-codeblock-creator.ts
@@ -3,7 +3,7 @@ import SmilesDrawerToObsidianPlugin from '../main'
 import {PluginSettings} from '../definitions/settings'
 import {ATOM_VISUALIZATION, MOLECULE_THEMES, SHOW_CARBONS} from '../definitions/smiles-drawer-adapter'
 import SmilesDrawer from 'smiles-drawer'
-import {GenericModal, GenericModalInput, UserInput, OutputData, NonExpandableInput} from '@Altarok/utils'
+import {GenericModal, GenericModalInput, UserInput, OutputData, NonExpandableInput} from '@Altarok/obsidian-dev-utils'
 
 export class CodeBlockCreatorModal extends Modal {
   constructor(public readonly app: App, public readonly plugin: SmilesDrawerToObsidianPlugin) {
