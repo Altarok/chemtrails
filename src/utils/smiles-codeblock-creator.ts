e17dc91886c64497b3dad5d23f4cad1efb0aaeb0
Altarok <thom.hammerklinge@gmail.com>
2026-07-21 11:13:26 +0200

    fix linked import

--- src/utils/smiles-codeblock-creator.ts
+++ src/utils/smiles-codeblock-creator.ts
@@ -3,7 +3,7 @@ import SmilesDrawerToObsidianPlugin from '../main'
 import {PluginSettings} from '../definitions/settings'
 import {ATOM_VISUALIZATION, MOLECULE_THEMES, SHOW_CARBONS} from '../definitions/smiles-drawer-adapter'
 import SmilesDrawer from 'smiles-drawer'
-import {GenericModal, MandatoryInput, OptionalInput, OutputData} from '@Altarok/obsidian-dev-utils/src'
+import {GenericModal, MandatoryInput, OptionalInput, OutputData} from '@Altarok/utils'
 
 // npm link/update @Altarok/obsidian-dev-utils
 export class CodeBlockCreatorModal extends Modal {
