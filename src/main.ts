9a9bba757effd1f800a6c601b6dfc3f2ac423914
Altarok <thom.hammerklinge@gmail.com>
2026-08-03 09:53:56 +0200

    change code according to new eslint rule

--- src/main.ts
+++ src/main.ts
@@ -101,7 +101,7 @@ export default class SmilesDrawerToObsidianPlugin extends Plugin {
     container.style.display = cssStyleDisplay
 
     /* Set up the target SVG element with responsive attributes */
-    const svgEl = window.document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    const svgEl = window.document.createSvg('svg')
     svgEl.setAttrs({'width': 'auto', 'height': localSettings.height})
 
     container.appendChild(svgEl)
